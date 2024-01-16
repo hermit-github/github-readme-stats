8c8c6fa4c17bcdc6cb9282bc7b39de2643815018
Shubham Halder <shubhamhalder0@gmail.com>
2024-01-16 10:24:21 +0530

    Express Setup

--- express.js
+++ express.js
@@ -7,10 +7,19 @@ import gistCard from "./api/gist.js";
 import express from "express";
 
 const app = express();
-app.listen(process.env.port || 9000);
+app.listen(process.env.port || 9000, () => {
+  console.log(`Server running on port:${process.env.PORT}`);
+});
 
-app.get("/", statsCard);
-app.get("/pin", repoCard);
-app.get("/top-langs", langCard);
-app.get("/wakatime", wakatimeCard);
-app.get("/gist", gistCard);
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to Hermit's Instance of Github Readme Stats",
+  });
+});
+
+app.get("/api", statsCard);
+app.get("/api/pin", repoCard);
+app.get("/api/top-langs", langCard);
+app.get("/api/wakatime", wakatimeCard);
+app.get("/api/gist", gistCard);
